3209e1b7afa954752f5fd5e2d90dd35db3672271
mewmew <rnd0x00@gmail.com>
2018-05-29 23:35:59 +0200

    move parsing to galaxy

--- cmd/mjolk/main.go
+++ cmd/mjolk/main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 
 	galaxy "github.com/godbit/Galaxy"
 	"github.com/godbit/Galaxy/knox"
-	"github.com/pkg/errors"
-	"github.com/simplereach/timeutils"
 )
 
 func main() {
 	flag.Parse()
 	for _, jsonPath := range flag.Args() {
-		events, err := parseJson(jsonPath)
+		events, err := galaxy.ParseFile(jsonPath)
 		if err != nil {
 			log.Fatalf("%+v", err)
 		}
@@ -36,36 +32,4 @@ func main() {
 		fmt.Println("E:", E)
 		fmt.Println("V:", V)
 	}
-}
-
-func parseJson(jsonPath string) ([]galaxy.Event, error) {
-	buf, err := ioutil.ReadFile(jsonPath)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	var data [][]interface{}
-	if err := json.Unmarshal(buf, &data); err != nil {
-		return nil, errors.WithStack(err)
-	}
-	var events []galaxy.Event
-	for _, d := range data {
-		event, err := parseEvent(d)
-		if err != nil {
-			return nil, errors.WithStack(err)
-		}
-		events = append(events, event)
-	}
-	return events, nil
-}
-
-func parseEvent(data []interface{}) (galaxy.Event, error) {
-	coords := data[2].([]interface{})
-	date, err := timeutils.ParseDateString(data[1].(string))
-	if err != nil {
-		return galaxy.Event{}, errors.WithStack(err)
-	}
-	return galaxy.Event{
-		T: date,
-		S: galaxy.Point{X: coords[0].(float64), Y: coords[1].(float64)},
-	}, nil
 }
