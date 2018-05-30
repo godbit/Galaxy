47fd18ade1a811829d1482dfd786362784000fe3
mewmew <rnd0x00@gmail.com>
2018-05-30 13:05:24 +0200

    rewrite inner loop in C; delta -94.4%
    [u@x220 galaxy]$ benchcmp old.txt new.txt
    benchmark              old ns/op      new ns/op     delta
    BenchmarkCluster-4     1658097373     92870884      -94.40%

--- galaxy/inner_cgo.go
+++ galaxy/inner_cgo.go
@@ -4,35 +4,93 @@ package galaxy
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"unsafe"
 
 	"github.com/karlek/progress/barcli"
 )
 
-// #cgo LDFLAGS: -lm
-//
-// #include <stdint.h>
-// #include <math.h>
-//
-// typedef struct {
-//    double X;
-//    double Y;
-// } Point;
-//
-// double ddiff_c(Point a, Point b) {
-//    double xdiff = a.X - b.X;
-//    double ydiff = a.Y - b.Y;
-//    return sqrt(xdiff*xdiff + ydiff*ydiff);
-// }
-//
-// int64_t tdiff_c(int64_t a, int64_t b) {
-//    if (a < b) {
-//       return b - a;
-//    }
-//    return a - b;
-// }
+/*
+#cgo LDFLAGS: -lm
+
+#include <stdint.h>
+#include <math.h>
+
+typedef struct {
+	double X;
+	double Y;
+} Point;
+
+double ddiff_c(Point a, Point b) {
+	double xdiff = a.X - b.X;
+	double ydiff = a.Y - b.Y;
+	return sqrt(xdiff*xdiff + ydiff*ydiff);
+}
+
+int64_t tdiff_c(int64_t a, int64_t b) {
+	if (a < b) {
+		return b - a;
+	}
+	return a - b;
+}
+
+typedef struct {
+	int Ns;
+	int N2s;
+	int Nt;
+	int N2t;
+	int X;
+} Result;
+
+typedef struct {
+	Point S;
+	int64_t T;
+} Event;
+
+#define Hour 3600000000000 // nanoseconds
+
+#define D_MAX 1800
+#define T_MAX (16*Hour*24 + Hour)
+
+Result inner(int imin, int imax, Event *events, int len) {
+	Result result;
+
+	for (int i = imin; i < imax; i++) {
+		for (int j = 0; j < len; j++) {
+			if (i == j) {
+				// this is just to eliminate the self-pairing, not that we are still double counting, i.e., both ij and ji are counted which we later have to normalize
+				continue;
+			}
+			double sdiff = ddiff_c(events[i].S, events[j].S);
+			if (sdiff <= D_MAX) {
+				result.Ns++;
+			}
+			double tdiff = tdiff_c(events[i].T, events[j].T);
+			if (tdiff <= T_MAX) {
+				result.Nt++;
+			}
+			if (sdiff <= D_MAX && tdiff <= T_MAX) {
+				result.X++;
+			}
+			for (int k = 0; k < len; k++) {
+				// the second order terms are also only double counted because the join of the pairs is only considered on j
+				if (i == k || j == k) {
+					continue;
+					// this is just to eliminate the self-pairing
+				}
+				if (sdiff <= D_MAX && ddiff_c(events[j].S, events[k].S) <= D_MAX) {
+					result.N2s++;
+				}
+				if (tdiff <= T_MAX && tdiff_c(events[j].T, events[k].T) <= T_MAX) {
+					result.N2t++;
+				}
+			}
+		}
+	}
+
+	return result;
+}
+*/
 import "C"
 
 func init() {
@@ -40,58 +98,13 @@ func init() {
 }
 
 func inner(ctx context.Context, imin, imax int, events []Event, bar *barcli.Bar, verbose bool, c chan Result) {
-	var result Result
-
-	for i := imin; i < imax; i++ {
-		// Send partial results on interrupt.
-		select {
-		case <-ctx.Done():
-			fmt.Println()
-			log.Printf("sending partial results for i = %d (%d iterations) in range [%d, %d)", i, i-imin, imin, imax)
-			c <- result
-			return
-		default:
-		}
-
-		if verbose {
-			bar.Inc()
-			// Only print status updates from the first worker Go routine.
-			if imin == 0 {
-				bar.Print()
-			}
-		}
-
-		for j := range events {
-			if i == j {
-				// this is just to eliminate the self-pairing, not that we are still double counting, i.e., both ij and ji are counted which we later have to normalize
-				continue
-			}
-			sdiff := float64(C.ddiff_c(*(*C.Point)(unsafe.Pointer(&events[i].S)), *(*C.Point)(unsafe.Pointer(&events[j].S))))
-			if sdiff <= dMax {
-				result.Ns++
-			}
-			tdiff := int64(C.tdiff_c(C.int64_t(events[i].T), C.int64_t(events[j].T)))
-			if tdiff <= tMax {
-				result.Nt++
-			}
-			if sdiff <= dMax && tdiff <= tMax {
-				result.X++
-			}
-			for k := range events {
-				// the second order terms are also only double counted because the join of the pairs is only considered on j
-				if i == k || j == k {
-					continue
-					// this is just to eliminate the self-pairing
-				}
-				if sdiff <= dMax && float64(C.ddiff_c(*(*C.Point)(unsafe.Pointer(&events[j].S)), *(*C.Point)(unsafe.Pointer(&events[k].S)))) <= dMax {
-					result.N2s++
-				}
-				if tdiff <= tMax && int64(C.tdiff_c(C.int64_t(events[j].T), C.int64_t(events[k].T))) <= tMax {
-					result.N2t++
-				}
-			}
-		}
+	r := C.inner(C.int(imin), C.int(imax), (*C.Event)(unsafe.Pointer(&events[0])), C.int(len(events)))
+	result := Result{
+		Ns:  int(r.Ns),
+		N2s: int(r.N2s),
+		Nt:  int(r.Nt),
+		N2t: int(r.N2t),
+		X:   int(r.X),
 	}
-
 	c <- result
 }
